d4411e99ef3bded5f5c00c43c3def7a2e7d21ba8
cj3022129 <cj3022129@LAB01-PC18>
2024-03-06 09:51:26 -0300

    Adicionar arquivos de projeto.

---
+++ ATV3_CJ3022129/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.ComponentModel.Design;
+
+float b1;
+float b2;
+float b3;
+float b4;
+float nota;
+
+
+Console.WriteLine("Digite a sua nota no primeiro bimestre: ");
+b1 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite a sua nota no segundo bimestre: ");
+b2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a sua nota no terceiro bimestre:");
+b3 = float.Parse(Console.ReadLine());
+Console.WriteLine("Digite a sua nota no quarto bimestre:");
+b4 = float.Parse(Console.ReadLine());
+
+nota= (b1 + b2+ b3 +b4) /4;
+if (nota <= 5.99)
+{
+    Console.WriteLine("O aluno está reprovado");
+  
+}
+
+
+else if (nota >= 6) 
+{
+    Console.WriteLine("O aluno foi aprovado");
+}
+
